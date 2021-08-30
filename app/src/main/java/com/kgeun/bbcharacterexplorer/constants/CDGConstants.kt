371d324c713f55c868a93c5852ac78cfb4e1a706
freebuds <ga77493@gmail.com>
2021-08-30 14:26:57 +0100

    - Implementing Season Filters

--- app/src/main/java/com/kgeun/bbcharacterexplorer/constants/CDGConstants.kt
+++ app/src/main/java/com/kgeun/bbcharacterexplorer/constants/CDGConstants.kt
@@ -2,10 +2,34 @@ package com.kgeun.bbcharacterexplorer.constants
 
 import com.kgeun.bbcharacterexplorer.BBApplication
 import com.kgeun.bbcharacterexplorer.R
+import com.kgeun.bbcharacterexplorer.data.model.ui.BBSeasonItem
 
 object CDGConstants {
     val ANIM_DURATION: Long = 800
     val SPLASH_LOGO_DURATION_1: Long = 500
     const val MAX_SHOWING_IMAGE: Int = 10
     val SERVER_SUCCESS = BBApplication.instance.applicationContext.getString(R.string.server_success)
+
+    val seasonItems = mutableMapOf<Int, BBSeasonItem>(
+        1 to BBSeasonItem(
+            "Season 1",
+            1
+        ),
+        2 to BBSeasonItem(
+            "Season 2",
+            2
+        ),
+        3 to BBSeasonItem(
+            "Season 3",
+            3
+        ),
+        4 to BBSeasonItem(
+            "Season 4",
+            4
+        ),
+        5 to BBSeasonItem(
+            "Season 5",
+            5
+        )
+    )
 }
---
+++ app/src/main/java/com/kgeun/bbcharacterexplorer/data/model/ui/BBSeasonItem.kt
@@ -0,0 +1,7 @@
+package com.kgeun.bbcharacterexplorer.data.model.ui
+
+data class BBSeasonItem(
+    val text: String = "",
+    val season: Int = 0,
+    var selected: Boolean = false
+)
