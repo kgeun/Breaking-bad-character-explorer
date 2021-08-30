73fbce7763e21dcd55e2d14f57a5c532193caba0
freebuds <ga77493@gmail.com>
2021-08-30 05:18:02 +0100

    - Implemented splash activity

---
+++ app/src/main/java/com/kgeun/bbcharacterexplorer/view/activity/BBSplashActivity.kt
@@ -0,0 +1,30 @@
+package com.kgeun.bbcharacterexplorer.view.activity
+
+import android.content.Intent
+import android.os.Bundle
+import androidx.lifecycle.lifecycleScope
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.launch
+import com.kgeun.bbcharacterexplorer.R
+import com.kgeun.bbcharacterexplorer.databinding.ActivitySplashBinding
+import com.kgeun.bbcharacterexplorer.view.CDGBaseActivity
+
+class BBSplashActivity : CDGBaseActivity() {
+
+    private val binding by lazy { ActivitySplashBinding.inflate(layoutInflater) }
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(binding.root)
+        lifecycleScope.launch {
+            delay(1000L)
+            moveToMain()
+        }
+    }
+
+    fun moveToMain() {
+        finish()
+        startActivity(Intent(this, BBMainActivity::class.java))
+        overridePendingTransition(R.anim.translate_fade_in, android.R.anim.fade_out)
+    }
+}
